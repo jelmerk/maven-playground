f23fc5621024ce11f7b0bbf0682d5d3fdbb8ea6b
Jelmer Kuperus <jkuperus@gmail.com>
2012-05-29 21:13:41 +0200

    javadoc

--- services-api/src/main/java/nl/orange11/service/HelloService.java
+++ services-api/src/main/java/nl/orange11/service/HelloService.java
@@ -1,10 +1,18 @@
 package nl.orange11.service;
 
 /**
+ * Simple service implementation.
+ *
  * @author Jelmer Kuperus
  */
 public interface HelloService {
 
+    /**
+     * Returns a message that says hello.
+     *
+     * @param name the name of the person to say hello to
+     * @return the created message
+     */
     String sayHello(String name);
 
 }
--- services-impl/src/main/java/nl/orange11/service/HelloServiceImpl.java
+++ services-impl/src/main/java/nl/orange11/service/HelloServiceImpl.java
@@ -3,10 +3,15 @@ package nl.orange11.service;
 import java.sql.ResultSet;
 
 /**
+ * Default implementation of the {@link HelloService} interface.
+ *
  * @author Jelmer Kuperus
  */
 public class HelloServiceImpl implements HelloService {
 
+    /**
+     * {@inheritDoc}
+     */
     public String sayHello(String name) {
         return "hello " + name  + ResultSet.TYPE_SCROLL_INSENSITIVE;
     }
